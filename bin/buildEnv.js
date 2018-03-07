10f09d217e04491acfb800bbf8f7a5fadefdfbee
Aaron Valdez <valdez2010@gmail.com>
2018-03-07 11:51:12 -0800

    Fixed buildEnv.

--- bin/buildEnv.js
+++ bin/buildEnv.js
@@ -3,7 +3,6 @@ const path = require("path");
 const envBuild = require("../config/.env.sample.js");
 
 const buildEnv = () => {
-<<<<<<< HEAD
   for (let pathname in envBuild) {
     fs.writeFileSync(__dirname + `/../${pathname}/.env`, "");
     envBuild[pathname].forEach(variable =>
@@ -14,12 +13,5 @@ const buildEnv = () => {
     );
   }
 };
-=======
-  _.each(envVariables, (value, key) => {
-    fs.writeFileSync(`./${key}/.env`, '')
-    createENVFile(key, value);
-  });
-}
->>>>>>> bca9b6a8ddf3b7589f2be503ca5e918fdbe3358a
 
 buildEnv();
