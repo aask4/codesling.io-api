7a3a7d0112bc390f5fe7c25cffb3020e2d3b5bc9
Kin Chan <xkinchanx@gmail.com>
2018-03-08 12:10:48 -0800

    [update]: changed forEach to for of loop to fix async issues with receiver

--- rest-server/src/components/histories/historyController.js
+++ rest-server/src/components/histories/historyController.js
@@ -21,7 +21,6 @@ export const historyController = async (req, res) => {
     return res.status(200).send();
   } catch (err) {
     error('historyController - error= ', err);
-    throw new Error(err);
   }
 };
 
@@ -30,10 +29,10 @@ import { fetchUserQuery } from '../users/userQueries';
 export const fetchHistoryController = async (req, res) => {
   try {
     const { rows } = await historyQueryHelper(req.params);
-    await rows.forEach(async (row) => {
-      user = await fetchUserQuery(row.receiver_id);
-      row.receiver = user;
-    });
+    for (let row of rows) {
+      let user = await fetchUserQuery({user_id: row.challenger_id});
+      row.receiver = user.rows[0];
+    }
     return res.status(200).send(rows);
   } catch (err) {
     error('error fetching messages ', err);
