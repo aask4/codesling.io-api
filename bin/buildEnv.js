0a2cb684cc810ca5d75d270547b1ba8074582e4f
qiang song <qiangsong889@yahoo.com>
2018-03-07 16:31:13 -0800

    good job

--- bin/buildEnv.js
+++ bin/buildEnv.js
@@ -10,23 +10,10 @@ const createENVFile = (directory, variables) => {
 }
 
 const buildEnv = () => {
-<<<<<<< HEAD
   _.each(envVariables, (value, key) => {
     fs.writeFileSync(`./${key}/.env`, '')
     createENVFile(key, value);
   });
 }
-=======
-  for (let pathname in envBuild) {
-    fs.writeFileSync(__dirname + `/../${pathname}/.env`, "");
-    envBuild[pathname].forEach(variable =>
-      fs.appendFileSync(
-        __dirname + `/../${pathname}/.env`,
-        variable + "\n"
-      )
-    );
-  }
-};
->>>>>>> 1a869833e606b9080a89f55fe35c6f96581fa45b
 
 buildEnv();
