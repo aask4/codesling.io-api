9960c1a287aa54f580f5415b323f2cc4946bbfb6
Alex Jaynes <dr.ocheyo@gmail.com>
2018-03-09 12:37:59 -0800

    Update openDuelsSQLHelpers.js

--- rest-server/src/components/openDuels/openDuelsSQLHelpers.js
+++ rest-server/src/components/openDuels/openDuelsSQLHelpers.js
@@ -1,7 +1,7 @@
-export const addOpenDuelHelper = ({challenge_id, challenger_id, sling_id}) => {
+export const addOpenDuelHelper = ({challenge_id, sling_id}) => {
   return `
     INSERT INTO openduels (challenge_id, challenger_id, sling_id)
-    VALUES ('${challenge_id}', '${challenger_id}', '${sling_id}')
+    VALUES ('${challenge_id}', '${sling_id}')
     RETURNING *
   `;
 };
@@ -9,7 +9,6 @@ export const addOpenDuelHelper = ({challenge_id, challenger_id, sling_id}) => {
 export const fetchOpenDuelHelper = () => {
   return `
     SELECT * FROM openduels
-    JOIN users ON openduels.challenger_id=users.id
     JOIN challenges ON openduels.challenge_id=challenges.id
   `;
 };
@@ -18,15 +17,7 @@ export const removeOpenDuelHelper = ({duel_id}) => {
   return `
     DELETE FROM openduels
     WHERE duel_id=${duel_id}
-    RETURNING challenge_id, challenger_id, opponent_id
+    RETURNING challenge_id
   `;
 };
 
-export const updateOpenDuelHelper = ({duel_id, opponent_id}) => {
-  return `
-    UPDATE openduels
-    SET opponent_id = '${opponent_id}'
-    WHERE duel_id = '${duel_id}'
-    RETURNING *
-  `
-};
